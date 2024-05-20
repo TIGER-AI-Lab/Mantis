84e9426048072940fc96f11f221704cba8b92ca9
Dongfu Jiang <dongfu.jdf@gmail.com>
2024-05-20 13:42:26 +0800

    update

--- mantis/train/scripts/ablations/train_mllava_data_ablation2.sh
+++ mantis/train/scripts/ablations/train_mllava_data_ablation2.sh
@@ -11,7 +11,7 @@ if [ "$HF_DATASETS_OFFLINE" = 1 ]; then
     echo "Warning: Offline mode is enabled. Using local copy of datasets"
     DATA_CONFIG_FILE="./data_configs/train_config_offline.yaml"
 else
-    DATA_CONFIG_FILE="./data_configs/mantis_coref_compare_config.yaml"
+    DATA_CONFIG_FILE="./data_configs/mllava_ablation2.yaml"  # change to this for offical training
 fi
 if [ "$TRANSFORMERS_OFFLINE" = 1 ]; then
     echo "Warning: Offline mode is enabled. Using local copy of models"
