297176aa53bdced31154e043be84ba7791edcd04
Dongfu <dongfu.jdf@gmail.com>
2024-05-20 14:36:10 -0400

    update

--- mantis/train/upload_checkpoint.py
+++ mantis/train/upload_checkpoint.py
@@ -11,18 +11,23 @@ def main(
     checkpoint_path:str=None,
     hub_token:str=os.environ.get("HF_TOKEN"),
     upload_mode="model_only",
-
+    
 ):
     checkpoint_path = Path(checkpoint_path)
     assert checkpoint_path.exists(), f"Checkpoint path {checkpoint_path} does not exist"
     checkpoint_steps = checkpoint_path.name.split("-")[-1]
     run_name = checkpoint_path.parent.name
-    args = torch.load(checkpoint_path / "training_args.bin")
+    
+    
+    if (checkpoint_path / "trainer_state.json").exists():
+        args = torch.load(checkpoint_path / "training_args.bin")
 
-    if args.hub_model_id is not None:
-        hub_model_id = args.hub_model_id + f"-{checkpoint_steps}"
+        if args.hub_model_id is not None:
+            hub_model_id = args.hub_model_id + f"-{checkpoint_steps}"
+        else:
+            hub_model_id = f"{repo}/{run_name}-{checkpoint_steps}"
     else:
-        hub_model_id = f"{repo}/{run_name}-{checkpoint_steps}"
+        hub_model_id = f"{repo}/{checkpoint_path.parent.name}"
 
     if upload_mode == "model_only":
         from mantis.models.mfuyu.processor import MFuyuProcessor
@@ -55,6 +60,18 @@ def main(
             repo_id=hub_model_id,
             repo_type="model"
         )
-    
+    elif upload_mode == "upload_folder":
+        from huggingface_hub import HfApi
+        api = HfApi()
+        for file in checkpoint_path.iterdir():
+            if file.is_file():
+                print(f"Uploading {file.name} to {hub_model_id}")
+                api.upload_file(
+                    file_path=str(file),
+                    path_in_repo=file.name,
+                    repo_id=hub_model_id,
+                    repo_type="model"
+                )
+            
 if __name__ == "__main__":
     fire.Fire(main)
