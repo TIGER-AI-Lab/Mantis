a84ea4884c5dcc736f83e22fb53a18a8ffcc4553
Dongfu Jiang <dongfu.jdf@gmail.com>
2024-05-30 11:08:58 +0800

    update

--- data/video_eval/prepare.py
+++ data/video_eval/prepare.py
@@ -1,7 +1,9 @@
 import json
 from pathlib import Path
 from tqdm import tqdm
-file_list = ["data_insf.json", "data_lab.json", "data_real.json", "data_static.json", "data_worsen_gen.json"]
+# file_list = ["data_insf.json", "data_lab.json", "data_real.json", "data_static.json", "data_worsen_gen.json"]
+file_list = list([str(x) for x in Path("./").glob("data*.json")])
+print(file_list)
 all_data = []
 for file_name in file_list:
     with open(file_name, "r") as f:
