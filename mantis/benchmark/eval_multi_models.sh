c15e5f0c881a5dd590c56e9808b38591fdf2d6de
Dongfu <dongfu.jdf@gmail.com>
2024-09-08 22:25:47 -0400

    change idefics3 processing

--- mantis/benchmark/eval_multi_models.sh
+++ mantis/benchmark/eval_multi_models.sh
@@ -47,6 +47,7 @@ export CUDA_VISIBLE_DEVICES=2 && bash eval_single_model.sh "mantis-8b-siglip-lla
 
 
 export CUDA_VISIBLE_DEVICES=0 && bash eval_single_model.sh "mantis-8b-idefics3_16384" &
+export CUDA_VISIBLE_DEVICES=1 && bash eval_single_model.sh "mantis-8b-idefics3_pure_16384" &
 export CUDA_VISIBLE_DEVICES=1 && bash eval_single_model.sh "idefics3" &
 
 
